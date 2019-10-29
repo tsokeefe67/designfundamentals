a9aac92ed413edbc078fb1a1b5459637b584b84d
tsokeefe67 <54561265+tsokeefe67@users.noreply.github.com>
2019-10-28 21:26:59 -0400

    Update pizza.php

--- pizza/pizza.php
+++ pizza/pizza.php
@@ -57,7 +57,7 @@
 <h2>Your pizza</h2>
 
 <?php if (!isset($_POST['crust']) && !isset($_POST['toppings']) && !isset($_POST['pizzas'])) { ?>
-<em>Sorry, we did not receive your information. <a href="http://www.blackgoosebistro.com/pizza.html">Try again.</a></em>
+<em>Sorry, we did not receive your information. <a href="http://https://tsokeefe67.github.io/designfundamentals/pizza/">Try again.</a></em>
 <?php } 
   else { ?>
     <ul>
